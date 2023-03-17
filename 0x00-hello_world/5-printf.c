952f90f5a19a75308466b447d6b98b75dfc43f5d
azazime <andrewasamonye@gmail.com>
2023-03-17 06:24:02 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - function within the main program
+ *
+ * return: return 0 always
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}	
